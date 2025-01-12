34a3e91a9ce8b62cf2f8f344a3a2c94b43cb5a35
Yevhenii <yevhenii.filonenko@modern-expo.com>
2025-01-12 18:11:09 +0200

    counter

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { addItem } from './CartSlice.jsx'
-import {useDispatch} from "react-redux";
+import {addItem} from './CartSlice.jsx'
+import {useDispatch, useSelector} from "react-redux";
 
 function ProductList() {
+    const cart = useSelector(state => state.cart.items);
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
@@ -255,6 +256,10 @@ function ProductList() {
         }))
     }
 
+    const isItemAdded = (itemName) => cart.find(({name}) => name === itemName);
+
+    const cartTotalAmount = () => cart.length;
+
     const handleContinueShopping = (e) => {
         e.preventDefault();
         setShowCart(false);
@@ -278,6 +283,7 @@ function ProductList() {
                     <div><a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div><a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
                         <h1 className='cart'>
+                            <div className="total_cart_amount">{cartTotalAmount()}</div>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor"
                                  height="68" width="68">
                                 <rect width="156" height="156" fill="none"></rect>
@@ -285,8 +291,8 @@ function ProductList() {
                                 <circle cx="184" cy="216" r="12"></circle>
                                 <path
                                     d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
-                                    fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" id="mainIconPathAttribute"></path>
+                                    fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round"
+                                    strokeWidth="2" id="mainIconPathAttribute"></path>
                             </svg>
                         </h1>
                     </a></div>
@@ -306,9 +312,10 @@ function ProductList() {
                                                 <div className="product-title">{plant.name}</div>
                                                 <div className="product-price">{plant.cost}</div>
                                                 <div>{plant.description}</div>
-                                                <button className="product-button"
-                                                        onClick={() => handleAddToCart(plant)}>Add to
-                                                    Cart
+                                                <button
+                                                    className={'product-button' + (isItemAdded(plant.name) ? ' added-to-cart' : '')}
+                                                    onClick={() => handleAddToCart(plant)}>
+                                                    {isItemAdded(plant.name) ? 'Added to Cart' : 'Add to Cart'}
                                                 </button>
                                             </div>
                                         ))
